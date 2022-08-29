169cc52e8478dea6c056d00ba53d3d748ff27655
PaulRomanov <pavel.ramanau@gmail.com>
2022-08-29 18:42:20 +0400

    add translate function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { environment } from './../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TranslatorAPI';
+  targetLanguage = '';
+  inputText = '';
+  transalatedText = '';
+
+constructor(private http: HttpClient) {
+  
+}
+
+translate() {
+  this.http.post("https://translation.googleapis.com/language/translate/v2?key="+environment.apiKey, 
+  {
+    "q": [this.inputText],
+    "target": this.targetLanguage
+  }).subscribe((response:any)=>{
+    this.transalatedText=response.data.translation[0].transalatedText
+  })
+}
+
 }
